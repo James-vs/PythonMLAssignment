d7040edae40b1fd5bec50f9258ee45ab986d64c3
jcvs1g20 <jcvs1g20@soton.ac.uk>
2023-05-26 12:51:11 +0100

    removed the randomforestclassifier and added an SVM classifier with a pipeline and grid search

--- Part A/PartA.py
+++ Part A/PartA.py
@@ -1,12 +1,17 @@
-import pandas
 from sklearn import preprocessing
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.model_selection import train_test_split
+from sklearn import svm, metrics
+from sklearn.pipeline import Pipeline
+from sklearn.model_selection import GridSearchCV
+from sklearn.preprocessing import StandardScaler
+import pandas as pd
+import numpy as np
+
 # from sklearn.discriminant_analysis import QuadraticDiscriminantAnalysis
 # from sklearn.gaussian_process import GaussianProcessClassifier
 # from sklearn.gaussian_process.kernels import RBF
 # from sklearn.linear_model import LogisticRegression
-
 # import numpy as np
 # from sklearn.naive_bayes import GaussianNB
 # from sklearn.neighbors import KNeighborsClassifier
@@ -15,21 +20,42 @@ from sklearn.model_selection import train_test_split
 # from sklearn.svm import SVC
 # from sklearn.tree import DecisionTreeClassifier
 
-trainingData = pandas.read_csv("TrainingDataBinary.csv", header=None)
-testingData = pandas.read_csv("TestingDataBinary.csv", header=None)
-# print(testingData)
+trainingData = pd.read_csv("TrainingDataBinary.csv", header=None)
+testingData = pd.read_csv("TestingDataBinary.csv", header=None)
 print(testingData.shape)
 print(trainingData.shape)
-# X = trainingData.iloc[:, 0:117].drop([1,6,7,8,12,13,14,15,16,17,19,20,28,30,31,37,39,41,42,44,45,48,50,51,54,55,56,58,68,71,75,77,80,81,83,84,85,86,90,91,93,97,100,101,106,107,108,111,112,113,114],axis=1)
 X = trainingData.iloc[:, 0:128]
 X = preprocessing.scale(X)
 y = trainingData.iloc[:, 128]
-# print(X)
+# print(trainingData[128].value_counts()) - proved that the data is balanced
+
+# creating an SVC classifier
+clf = svm.SVC()
+
+# creating a pipeline
+pipe = Pipeline(steps=[('scaler', StandardScaler()), ('svm', clf)])
+
+# Split dataset into training set and test set
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.25, random_state=30)
+# 70% training and 30% test
+# randomise/shuffle 30 data points when splitting the data
 
 
-while True:
-    X_train, X_test, y_train, y_test = train_test_split(X, y, train_size=0.8, test_size=0.2)
-    model = RandomForestClassifier()
-    model.fit(X_train, y_train.values.ravel())
-    print(model.score(X_test, y_test))
-    print(model.predict(testingData))
+# defining a grid search and parameters for finding optimal hyperparameters
+parameters = {
+    "svm__kernel": ['rbf'], # 'linear', 'poly',
+    "svm__C": [1, 1000, 1500, 2000], # 0.1, 0.01, 10,1, 100,  , 1e5 0.001,0.5e4, 1e4, 1.5e4
+    "svm__gamma": [0.17], # 0.5, 0.1, , 0.025, 0.05
+}
+search = GridSearchCV(pipe, param_grid=parameters, n_jobs=-1, cv=10) # cv changed from 10
+search.fit(X_train, y_train)
+
+# printing the accuracy and best parameters
+print("Best parameter (CV score=%0.5f):" % search.best_score_)
+print(search.best_params_)
+
+# while True:
+# model = RandomForestClassifier()
+# model.fit(X_train, y_train.values.ravel())
+# print(model.score(X_test, y_test))
+# print(model.predict(testingData))
