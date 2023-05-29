4b99b58201a0f7d9e0d8a5e68135ac320e3c4a02
jcvs1g20 <jcvs1g20@soton.ac.uk>
2023-05-29 19:55:59 +0100

    Tuned hyperparameters for rfc to a cv best score of .988

--- Part A/PartA-RFC.py
+++ Part A/PartA-RFC.py
@@ -18,7 +18,7 @@ y = trainingData.iloc[:, 128]
 # print(trainingData[128].value_counts()) - proved that the data is balanced
 
 # creating a RandomForestClassifier variable
-rfc = RandomForestClassifier(random_state=30)
+rfc = RandomForestClassifier()
 # using 30 as the random state value since this worked best in the SVC model
 
 # creating a pipeline
@@ -32,20 +32,23 @@ X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.25, random
 
 # defining a grid search and parameters for finding optimal hyperparameters
 parameters = {
-    'n_estimators': [100, 200, 500], #
-    'max_features': ['sqrt', 'log2']# ,
-    ##'max_depth': [4, 5, 6, 7, 8],
-    ##'criterion': ['gini', 'entropy']
+    'n_estimators': [100], # 50, 150, 200, 500
+    'max_features': ['log2'], # , 'sqrt'
+    'criterion': ['entropy'], # 'gini', 'log_loss'
+    'random_state': [30] # 20, 25, 35, 40
 }
 cv_rfc = GridSearchCV(estimator=rfc, param_grid=parameters, n_jobs=-1, cv=10) # 'pipe,' removed for rfc testing
-cv_rfc.fit(X_train, y_train)
+cv_rfc.fit(X_train, y_train.values.ravel())
 
 # printing the accuracy and best parameters
 print("Best parameter (CV score=%0.5f):" % cv_rfc.best_score_)
 print(cv_rfc.best_params_)
+print("Applying model to testing data")
+print("Testing Accuracy: " + str(cv_rfc.score(X_test, y_test)))
+print("Applying Model to unseen data")
+print(cv_rfc.predict(testingData))
 
 # while True:
 # model = RandomForestClassifier()
 # model.fit(X_train, y_train.values.ravel())
-# print(model.score(X_test, y_test))
-# print(model.predict(testingData))
+
