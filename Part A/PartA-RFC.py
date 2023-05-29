a2b96249a398709e42987543dc45ec508ae47bff
jcvs1g20 <jcvs1g20@soton.ac.uk>
2023-05-29 20:13:30 +0100

    added and removed logistic regression, will try to add a PCA step to the model next

--- Part A/PartA-RFC.py
+++ Part A/PartA-RFC.py
@@ -1,7 +1,7 @@
 from sklearn import preprocessing
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.model_selection import train_test_split
-from sklearn import svm, metrics
+from sklearn import metrics
 from sklearn.pipeline import Pipeline
 from sklearn.model_selection import GridSearchCV
 from sklearn.preprocessing import StandardScaler
@@ -22,7 +22,7 @@ rfc = RandomForestClassifier()
 # using 30 as the random state value since this worked best in the SVC model
 
 # creating a pipeline
-# pipe = Pipeline(steps=[('scaler', StandardScaler()), ('svm', clf)])
+pipe = Pipeline(steps=[('scaler', StandardScaler()), ('rfc', rfc)])
 
 # Split dataset into training set and test set
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.25, random_state=30)
@@ -32,12 +32,12 @@ X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.25, random
 
 # defining a grid search and parameters for finding optimal hyperparameters
 parameters = {
-    'n_estimators': [100], # 50, 150, 200, 500
-    'max_features': ['log2'], # , 'sqrt'
-    'criterion': ['entropy'], # 'gini', 'log_loss'
-    'random_state': [30] # 20, 25, 35, 40
+    'rfc__n_estimators': [100], # 50, 150, 200, 500
+    'rfc__max_features': ['log2'], # , 'sqrt'
+    'rfc__criterion': ['entropy'], # 'gini', 'log_loss'
+    'rfc__random_state': [30] # 20, 25, 35, 40
 }
-cv_rfc = GridSearchCV(estimator=rfc, param_grid=parameters, n_jobs=-1, cv=10) # 'pipe,' removed for rfc testing
+cv_rfc = GridSearchCV(pipe, param_grid=parameters, n_jobs=-1, cv=10) # estimator=rfc removed for pipe testing
 cv_rfc.fit(X_train, y_train.values.ravel())
 
 # printing the accuracy and best parameters
