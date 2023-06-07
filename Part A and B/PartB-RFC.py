6f732791644affceee4c00456ddfde0fd87a6ab2
jcvs1g20 <jcvs1g20@soton.ac.uk>
2023-06-07 23:29:13 +0100

    Adding final comments to the code + streamlining spaces and indentation

--- Part A and B/PartB-RFC.py
+++ Part A and B/PartB-RFC.py
@@ -1,68 +1,55 @@
-from sklearn import preprocessing
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.model_selection import train_test_split
-from sklearn import metrics
 from sklearn.metrics import confusion_matrix, ConfusionMatrixDisplay, f1_score
-from sklearn.pipeline import Pipeline
 from sklearn.model_selection import GridSearchCV
-from sklearn.preprocessing import StandardScaler, MinMaxScaler
 import pandas as pd
-import numpy as np
 import matplotlib.pyplot as plt
 
+# importing the training and testing data into DataFrames
 trainingData = pd.read_csv("TrainingDataMulti.csv", header=None)
 testingData = pd.read_csv("TestingDataMulti.csv", header=None)
 print(testingData.shape)
 print(trainingData.shape)
 X = trainingData.iloc[:, 0:128]
-# X = preprocessing.scale(X)
 y = trainingData.iloc[:, 128]
-# print(trainingData[128].value_counts()) - proved that the data is balanced
 
 # creating a RandomForestClassifier variable
 rfc = RandomForestClassifier()
-# using 30 as the random state value since this worked best in the SVC model
 
-# creating a pipeline
-# pipe = Pipeline(steps=[('scaler', MinMaxScaler()),('rfc', rfc)]) # StandardScaler()
-
-# Split dataset into training set and test set
+# split dataset into training set and test set
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.25, random_state=30)
-# 75% training and 25% test
-# randomise/shuffle 30 data points when splitting the data
-
+#   - 75% training and 25% test, seed random number generator with 30 for consistency across executions
 
 # defining a grid search and parameters for finding optimal hyperparameters
 params = {
-    'n_estimators': [500], # 50, 100, 200, 500, 1000
+    'n_estimators': [500], # 50, 100, 200, 500, 1000            - values included in initial grid search
     'max_features': ['log2'], # 'log2', 'sqrt'
     'criterion': ['entropy'], # 'entropy', 'gini', 'log_loss'
     'random_state': [30] # 20, 30, 42
 }
-
 cv_rfc = GridSearchCV(estimator=rfc, param_grid=params, n_jobs=-1, cv=10)
 cv_rfc.fit(X_train, y_train)
 
-# printing the accuracy and best parameters
-print("Best parameter (CV score=%0.5f):" % cv_rfc.best_score_)
-print(cv_rfc.best_params_)
+# applying model to testing data
 print("Applying model to testing data")
 y_pred = cv_rfc.predict(X_test)
-# print("Accuracy score: " + str(metrics.accuracy_score(y_test, y_pred)))
+
+# generating F1 score and training accuracy score
 print("F1 Score: " + str(f1_score(y_test, y_pred, average='macro')))
 print("Training Accuracy: " + str(cv_rfc.score(X_test, y_test)))
+
+# applying the model to the unseen test data
 print("Applying Model to unseen data")
 predLabels = cv_rfc.predict(testingData)
 print(predLabels)
 
+# output the results to a csv file
 outputData = testingData
 outputData[128] = predLabels
 outputData.to_csv("TestingResultsMulti.csv", header=False, index=False)
 
-# get confusion matrix
+# generating confusion matrix
 cm = confusion_matrix(y_test, y_pred, labels=cv_rfc.classes_)
 disp = ConfusionMatrixDisplay(confusion_matrix=cm, display_labels=cv_rfc.classes_)
 disp.plot()
 plt.show()
-
-
